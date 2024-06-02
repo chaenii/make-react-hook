9316c8d6fd3bbe0635d50f994608368613985c34
chaenii <chaenii.jeon@gmail.com>
2024-06-02 20:57:05 +0900

    add useClick Hook\

--- useClick/App.js
+++ useClick/App.js
@@ -1,1 +1,14 @@
 import React from "react";
+import useClick from "./useClick";
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
