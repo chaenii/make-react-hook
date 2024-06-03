238d06edd016dec8f7377981f6efb2ae9a4cf3b6
chaenii <chaenii.jeon@gmail.com>
2024-06-03 23:20:15 +0900

    add useScroll hook

---
+++ useScroll/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import useScroll from "./useScroll";
+
+const App = () => {
+  const { y } = useScroll();
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
